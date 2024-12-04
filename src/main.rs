f04687a4eb60b2d71b27d5915e727380aa79758c
Cielcee <celiacie099@gmail.com>
2024-12-04 10:42:11 -0500

    Initial

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+//150 lines not including test, multiple files (seperated read files)
+//initialize on github
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ target/debug/deps/FINAL_PROJECT-0d339ae3d9a4a67c.d
@@ -0,0 +1,5 @@
+/Users/gaoyuwei/Desktop/BU CDS/DS210/Final Project/FINAL_PROJECT/target/debug/deps/libFINAL_PROJECT-0d339ae3d9a4a67c.rmeta: src/main.rs
+
+/Users/gaoyuwei/Desktop/BU CDS/DS210/Final Project/FINAL_PROJECT/target/debug/deps/FINAL_PROJECT-0d339ae3d9a4a67c.d: src/main.rs
+
+src/main.rs:
